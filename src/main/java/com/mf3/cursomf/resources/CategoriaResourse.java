aed1bcd5f76ba5e90d7f93f8a8f2adc2c2ee16bb
Melquisedec Filipe (DpSI) <melquisedecfilipe@hotmail.com>
2018-12-10 13:25:23 +0100

    Testando Primeira Classe de Dominio Categoria

---
+++ src/main/java/com/mf3/cursomf/resources/CategoriaResourse.java
@@ -0,0 +1,31 @@
+package com.mf3.cursomf.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mf3.cursomf.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResourse {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1= new Categoria(1,"Informatica");
+		Categoria cat2= new Categoria(2,"Escritorio");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
+		
+		
+	}
+	
+}
